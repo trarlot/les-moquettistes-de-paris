2b9f039abdc2104d5978a73b261fcf8d70d024a1
Tristan Arlot <tristan45220@hotmail.fr>
2025-12-04 23:27:56 +0100

    last modif

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
                     <div className="flex flex-col lg:flex-row-reverse gap-6 sm:gap-8 items-center">
                         <div className="relative  overflow-hidden w-full max-w-sm lg:max-w-md aspect-square shrink-0">
                             <Image
-                                src="/assets/moquettistes-paris-logo.png"
+                                src="/assets/logo-moquettistes-paris.png"
                                 alt="Réalisation de pose de moquette professionnelle"
                                 className="object-cover w-full h-full"
                                 width={600}
